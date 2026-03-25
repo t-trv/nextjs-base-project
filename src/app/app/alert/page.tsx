4cde0b6113cd74b65f57d71dc471f473ac87d9b1
trv <trhuvan@gmail.com>
2026-03-25 23:10:49 +0700

    update: alert page

--- src/app/app/alert/page.tsx
+++ src/app/app/alert/page.tsx
@@ -1,3 +1,15 @@
+import AlertList from './_components/AlertList';
+import ControlBtn from './_components/ControlBtn';
+
 export default function AlertPage() {
-  return <div>AlertPage</div>;
+  return (
+    <div className="p-4 space-y-4">
+      {/* Heading of page */}
+      <h1 className="text-xl font-semibold">Danh sách cảnh báo từ camera</h1>
+
+      <ControlBtn />
+
+      <AlertList />
+    </div>
+  );
 }
